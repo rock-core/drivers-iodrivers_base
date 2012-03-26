78aa093101c72aaec29135744fa2c3e85458d040
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2012-03-26 13:39:47 +0200

    Added TCPServer support to iodriver

---
+++ src/TCPDriver.cpp
@@ -0,0 +1,116 @@
+#include "TCPDriver.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <signal.h>
+
+
+namespace iodrivers_base{
+
+TCPDriver::TCPDriver(int max_packet_size, bool extract_last): 
+    Driver(max_packet_size,extract_last)
+{
+    if(signal(SIGPIPE, SIG_IGN)){
+        throw iodrivers_base::UnixError("TCPDriver: Could not deactivate signals");
+    }
+
+}
+
+TCPDriver::~TCPDriver(){
+    ::close(socked_fd);
+    if(client_fd)
+        ::close(client_fd);
+
+}
+
+void TCPDriver::tcp_server_init(int port){
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+    socked_fd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if (socked_fd < 0) 
+        throw iodrivers_base::UnixError("TCPDriver: Could not create socked");
+
+    if (bind(socked_fd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){ 
+        throw iodrivers_base::UnixError("TCPDriver: Could bind to socked");
+    }
+
+    listen(socked_fd,5);
+    clilen = sizeof(cli_addr);
+    client_fd = 0;
+    fcntl(socked_fd,F_SETFL,O_NONBLOCK);
+}
+    
+    
+void TCPDriver::checkClientConnection(){
+  int new_client= accept(socked_fd, (struct sockaddr *) &cli_addr, &clilen);
+  if(new_client > 0){
+      if(client_fd)
+          ::close(client_fd);
+      client_fd = new_client;
+      setFileDescriptor(new_client,false);
+  }
+}
+        
+
+int TCPDriver::readPacket(uint8_t* buffer, int bufsize){
+    checkClientConnection();
+    if(client_fd)
+        return iodrivers_base::Driver::readPacket(buffer,bufsize);
+    else 
+        return 0;
+}
+    
+int TCPDriver::readPacket(uint8_t* buffer, int bufsize, base::Time const& packet_timeout, base::Time const& first_byte_timeout){
+    checkClientConnection();
+    if(client_fd){
+        int res = 0;
+        try{
+            res =  iodrivers_base::Driver::readPacket(buffer,bufsize,packet_timeout, first_byte_timeout);
+            return res;
+        }catch(iodrivers_base::UnixError e){
+            if(e.error == 32){ //Broken PIPE
+                ::close(client_fd);
+                client_fd = 0;
+                return 0;
+            }else{
+                printf("Error is: %i\n",e.error);
+                throw e;
+            }
+        }
+    }else 
+        return 0;
+}
+bool TCPDriver::writePacket(uint8_t const* buffer, int bufsize, base::Time const& timeout){
+    checkClientConnection();
+    if(client_fd){
+        bool res = false;
+        try{
+            res =  iodrivers_base::Driver::writePacket(buffer,bufsize,timeout);
+            return res;
+        }catch(iodrivers_base::UnixError e){
+            if(e.error == 32){ //Broken PIPE
+                ::close(client_fd);
+                client_fd = 0;
+                return false;
+            }else{
+                printf("Error is: %i\n",e.error);
+                throw e;
+            }
+        }
+    }else
+        return false;
+}
+
+
+};
---
+++ src/TCPDriver.hpp
@@ -0,0 +1,73 @@
+#include <iodrivers_base/Driver.hpp>
+#include <netinet/in.h>
+
+namespace iodrivers_base{
+
+/**
+ * This class implements an TCP Server for single Client
+ * If more thn one client tryes to connect, the old one is disconnected
+ * see checkClientConnection for more details or if you want to implement support
+ * for more than one client
+ */
+
+class TCPDriver : public iodrivers_base::Driver {
+    public:
+
+        TCPDriver(int max_packet_size, bool extract_last = false);
+        virtual ~TCPDriver();
+        
+        
+        /**
+         * Initialized an new soked so that an TCP client can connect to the given port,
+         * connection is not established by this mehtod, you need to call read or write packed
+         * only one time to get the first connection.
+         */
+        void tcp_server_init(int port);
+
+        /**
+         * Overloaded method from iodriver_base::Driver, additionally calls checkClientConntion();
+         */
+        virtual int readPacket(uint8_t* buffer, int bufsize);
+        virtual int readPacket(uint8_t* buffer, int bufsize, base::Time const& packet_timeout, base::Time const& first_byte_timeout);
+        
+        /**
+         * Overloaded method from iodriver_base::Driver, additionally calls checkClientConntion();
+         */
+        virtual bool writePacket(uint8_t const* buffer, int bufsize, base::Time const& timeout);
+    
+    protected:
+        /**
+         * This Method cheks for an new waiting client that tryes to connect to the current port.
+         * If an new clienet is discoverd, the old one will be disconnected and an connection
+         * to the new one is established
+         * The new fiile descriptor is passed to the iodriver to now handle the connection to the new one
+         * the old one is closed so that the old client will be disconnected
+         */
+        virtual void checkClientConnection();
+
+        /**
+         * Corresponding file descriptor to the socked
+         */
+        int socked_fd;
+
+        /*
+         * this member could be also handled by the Driver class itsel, but not sure what the Driver do internally, 
+         * so keep this member for know inside of this class
+         */
+        int client_fd;
+
+        /**
+         * Internal members to handle the connection
+         */
+        struct sockaddr_in cli_addr;
+        
+        /**
+         * Internal members to handle the connection
+         */
+        socklen_t clilen;
+
+};
+
+
+
+};
