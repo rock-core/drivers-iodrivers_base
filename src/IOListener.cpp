7eb9319bb6c56ea38cbc888de8e38e57ae5072cd
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-03-13 13:45:59 +0100

    explicid usage of boost::uint8_t instead of 'uint8_t', which is also defined in cstdint of gcc4.8

--- src/IOListener.cpp
+++ src/IOListener.cpp
@@ -1,19 +1,19 @@
 #include <iodrivers_base/IOListener.hpp>
 
 using namespace iodrivers_base;
-using boost::uint8_t;
+
 
 IOListener::~IOListener() {}
 
-std::vector<uint8_t> BufferListener::flushRead()
+std::vector<boost::uint8_t> BufferListener::flushRead()
 {
-    std::vector<uint8_t> ret;
+    std::vector<boost::uint8_t> ret;
     ret.swap(m_readBuffer);
     return ret;
 }
-std::vector<uint8_t> BufferListener::flushWrite()
+std::vector<boost::uint8_t> BufferListener::flushWrite()
 {
-    std::vector<uint8_t> ret;
+    std::vector<boost::uint8_t> ret;
     ret.swap(m_writeBuffer);
     return ret;
 }
@@ -21,13 +21,13 @@ std::vector<uint8_t> BufferListener::flushWrite()
 /** Used to pass data that has been written to the device to the
  * listener
  */
-void BufferListener::writeData(uint8_t const* data, size_t size)
+void BufferListener::writeData(boost::uint8_t const* data, size_t size)
 {
     m_writeBuffer.insert(m_writeBuffer.end(), data, data + size);
 }
 /** Used to pass data that has been read from the device to the listener
 */
-void BufferListener::readData(uint8_t const* data, size_t size)
+void BufferListener::readData(boost::uint8_t const* data, size_t size)
 {
     m_readBuffer.insert(m_readBuffer.end(), data, data + size);
 }
