9d5b26b33b0c585ffc1026e0c49b3669519fd917
Gabriel Arjones <arjones@arjones.com>
2016-08-29 11:52:38 -0300

    Fix tests

--- test/test.cpp
+++ test/test.cpp
@@ -315,7 +315,7 @@ BOOST_AUTO_TEST_CASE(test_open_bidirectional_udp)
 {
     DriverTest test;
 
-    BOOST_REQUIRE_NO_THROW(test.openURI("udp://127.0.0.1:3135:4145"));
+    BOOST_REQUIRE_NO_THROW(test.openURI("udp://127.0.0.1:1111:2222"));
     test.close();
 }
 
@@ -324,7 +324,7 @@ void recv_test()
     DriverTest test;
     uint8_t msg[4] = { 0, 'a', 'b', 0 };
 
-    test.openURI("udp://127.0.0.1:4145");
+    test.openURI("udp://127.0.0.1:2125");
 
     test.writePacket(msg, 4);
     test.close();
@@ -335,15 +335,18 @@ BOOST_AUTO_TEST_CASE(test_recv_from_bidirectional_udp)
     DriverTest test;
     uint8_t buffer[100];
     int count;
+    uint8_t msg[4] = { 0, 'a', 'b', 0 };
     boost::thread send_thread;
 
-    BOOST_REQUIRE_NO_THROW(test.openURI("udp://127.0.0.1:3135:4145"));
+    BOOST_REQUIRE_NO_THROW(test.openURI("udp://127.0.0.1:3135:2125"));
 
     send_thread = boost::thread(recv_test);
 
     BOOST_REQUIRE_NO_THROW((count = test.readPacket(buffer, 100, 200)));
     BOOST_REQUIRE_EQUAL(count, 4);
+    BOOST_REQUIRE_EQUAL(memcmp(buffer, msg, count), 0);
 
+    send_thread.join();
     test.close();
 }
 
@@ -351,19 +354,21 @@ void send_test(bool *got)
 {
     DriverTest test;
     uint8_t buffer[100];
+    uint8_t msg[4] = { 0, 'a', 'b', 0 };
     int count = 0;
 
-    test.openURI("udpserver://5155");
+    test.openURI("udpserver://4145");
 
     try {
-        count = test.readPacket(buffer, 100, 200);
+        count = test.readPacket(buffer, 100, 500);
     }
-    catch (...) {
+    catch (std::runtime_error e) {
         *got = false;
+        test.close();
+        return;
     }
 
-    *got = (count == 4);
-    cout << "Got: " << count << endl;
+    *got = ((count == 4) && (memcmp(buffer, msg, count) == 0));
     test.close();
 }
 
@@ -373,11 +378,10 @@ BOOST_AUTO_TEST_CASE(test_send_from_bidirectional_udp)
     uint8_t msg[4] = { 0, 'a', 'b', 0 };
     bool got = false;
 
-    boost::thread recv_thread;
+    boost::thread recv_thread(send_test, &got);;
 
-    BOOST_REQUIRE_NO_THROW(test.openURI("udp://127.0.0.1:5155:3135"));
-
-    recv_thread = boost::thread(send_test, &got);
+    BOOST_REQUIRE_NO_THROW(test.openURI("udp://127.0.0.1:4145:5155"));
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
 
     BOOST_REQUIRE_NO_THROW(test.writePacket(msg, 4));
 
